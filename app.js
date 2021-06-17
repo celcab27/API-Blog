de1983ef62773a4cb0931f36233af7c012eaf7cb
Celeste <celestecabrera.2711@gmail.com>
2021-06-17 12:40:01 -0300

    Base de datos y tablas conectadas

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
-
+const cors = require('cors');
 
 const app = express();
+
+require('./db');
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(5500, () =>
 {
