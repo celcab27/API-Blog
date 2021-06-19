e0835fb829de7b697f63713951d536125f099543
Celeste <celestecabrera.2711@gmail.com>
2021-06-18 23:31:12 -0300

    Imagenes validadas con expresiones regulares. La imagen debe terminar si o si en jpg o png

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,59 +1,62 @@
-const {Post} = require('./../db');
+const { Post } = require("./../db");
 
 module.exports = {
-    async getPosts (req, res)
-    {
-        var posts = await Post.findAll(
-            {
-                attributes: ['id', 'title', 'image', 'category', 'creationDate']
-            }
-        );
-        
-        posts = posts.sort((a, b) => b.creationDate > a.creationDate);
-        res.json(posts);
-    },
+  async getPosts(req, res) {
+    var posts = await Post.findAll({
+      attributes: ["id", "title", "image", "category", "creationDate"],
+    });
 
-    async postAPost(req, res)
-    {
-        const {title, content, category, image, creationDate} = req.body;
+    posts = posts.sort((a, b) => b.creationDate > a.creationDate);
+    res.json(posts);
+  },
 
-        if(title && content && category && image && creationDate)
-        {
-            const post = Post.create(req.body);
+  async postAPost(req, res) {
+    const { title, content, category, image, creationDate } = req.body;
 
-            res.json(post);
-        }
-        else
-        {
-            res.json("Error. Faltan propiedades");
-        }
-    },
+    if (title && content && category && image && creationDate) {
 
-    async updatePost(req, res)
-    {
-            const update = req.body;
-            Post.update(
-              update,
-              {
-                where: { id: req.params.id },
-              }
-            )
-            .then( () => {
-                res.json('El post ha sido actualizado correctamente');
-            })
-            .catch( (err) =>
-            {
-                console.log(err);
-                res.json({Error: err})
-            })          
-    },
+      const imageValidation = validarImagen(image);
 
-    async deletePost(req, res)
-    {
-        await Post.destroy({
-            where: {id: req.params.id}
-        });
-        
-        res.json({Succes: 'Post has been deleted'});
+      if (!imageValidation) {
+        console.log("Imagen no validada");
+        res.json("Error. El dato ingresado no es una imagen");
+      } else {
+        const post = Post.create(req.body);
+        res.json({ Post: post });
+      }
+    } else {
+      res.json("Error. Faltan propiedades");
     }
+  },
+
+  async updatePost(req, res) {
+    const update = req.body;
+    Post.update(update, {
+      where: { id: req.params.id },
+    })
+      .then(() => {
+        res.json("El post ha sido actualizado correctamente");
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ Error: err });
+      });
+  },
+
+  async deletePost(req, res) {
+    await Post.destroy({
+      where: { id: req.params.id },
+    });
+
+    res.json({ Succes: "Post has been deleted" });
+  },
+};
+
+function validarImagen(url) {
+  const regExp = /\.(jpg|png|)$/i;
+  if (!regExp.test(url)) {
+    return false;
+  } else {
+    return true;
+  }
 }
